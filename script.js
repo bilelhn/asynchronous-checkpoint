c82eb17c8602d70b357baf4f3d44a000efbdb711
bilelhn <hannafi.bilel@gmail.com>
2024-05-14 18:49:13 +0000

    second

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //------------- task 1-------------
-async function iterateWithAsyncAwait(arr){
-    for(let i=0;i<arr.length;i++){
-        setTimeout(() => {
-           console.log(arr[i]);
-        }, 1000*i);
-    }
+// async function iterateWithAsyncAwait(arr){
+//     for(let i=0;i<arr.length;i++){
+//         setTimeout(() => {
+//            console.log(arr[i]);
+//         }, 1000*i);
+//     }
+// }
+// iterateWithAsyncAwait(['a1','a2','a3']);
+
+
+// ------------ task 2 ------------
+async function awaitCall(){
+    let data = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(await data.json() );
 }
-iterateWithAsyncAwait(['a1','a2','a3']);
+awaitCall();
